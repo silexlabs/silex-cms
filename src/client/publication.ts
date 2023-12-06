584e2232a901f0c8faafe448a04c4227baafa2b2
Alex Hoyau <a.hoyau@silexlabs.org>
2023-12-06 17:24:36 +0300

    ncu

--- src/client/publication.ts
+++ src/client/publication.ts
@@ -281,14 +281,18 @@ function queryToDataFile(dataSource: IDataSourceModel, queryStr: string, options
   const url = s2s ? s2s.url : dataSource.get('url')
   const method = s2s ? s2s.method : dataSource.get('method')
   const headers = s2s ? s2s.headers : dataSource.get('headers')
-  const headersStr = headers ? Object.entries(headers).map(([key, value]) => `'${key}': '${value}'`).join('\n') : ''
+  // Check that the content-type is set
+  if(headers && !headers['content-type']) {
+    console.warn('11ty plugin for Silex: no content-type in headers of the graphql query. I will set it to application/json for you. To avoid this warning, add a header with key "content-type" and value "application/json" in silex config.')
+    headers['content-type'] = 'application/json'
+  }
+  const headersStr = headers ? Object.entries(headers).map(([key, value]) => `'${key}': '${value}',`).join('\n') : ''
   return `
-  result['${dataSource.id}'] = (await EleventyFetch('${url}', {
+  result['${dataSource.id}'] = (await EleventyFetch(\`${url}\`, {
     type: 'json',
     ${options.fetchPlugin ? `...${JSON.stringify(options.fetchPlugin)},` : ''}
     fetchOptions: {
       headers: {
-        'Content-Type': 'application/json',
         ${headersStr}
       },
       method: '${method}',
