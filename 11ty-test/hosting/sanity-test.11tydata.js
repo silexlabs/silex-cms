9181aa46c3720f85d6d02e66d776fdb6fbfa82af
Alex Hoyau <a.hoyau@silexlabs.org>
2023-12-28 11:28:02 +0300

    sanity compatible

---
+++ 11ty-test/hosting/sanity-test.11tydata.js
@@ -0,0 +1,62 @@
+
+const EleventyFetch = require('@11ty/eleventy-fetch')
+module.exports = async function () {
+  const result = {}
+  
+  try {
+    result['sanity'] = (await EleventyFetch(`https://gl93e3h9.api.sanity.io/v2023-08-01/graphql/production/default`, {
+      type: 'json',
+      ...{"duration":"0s","type":"json"},
+      fetchOptions: {
+        headers: {
+          'Authorization': `Bearer skGP3qsWCXgPPTqmvIyAdeFhACwWKt94Eq3oCexQNvalA291L03mfLLq0DFHN9pVxhPuTVSs5vfDucKpWZHKRGGKwBdou4QYW32yNBBkHJjl2QGnn67KtdaqRCo7vhxlBObbZdFVylCgtO2M6c4lvkal0owBpVO4PCcMxeUC38z1r8pjvTXJ`,
+'content-type': `application/json`,
+        },
+        method: 'POST',
+        body: JSON.stringify({
+          query: `query {
+  __typename
+  allBlog {
+    __typename
+    author {
+      __typename
+      name
+      avatar {
+        __typename
+        asset {
+          __typename
+          url
+        }
+      }
+    }
+    title
+  }
+}`,
+        })
+      }
+    })).data
+  } catch (e) {
+    console.error('11ty plugin for Silex: error fetching graphql data', e, 'sanity', 'https://gl93e3h9.api.sanity.io/v2023-08-01/graphql/production/default', 'POST', `query {
+  __typename
+  allBlog {
+    __typename
+    author {
+      __typename
+      name
+      avatar {
+        __typename
+        asset {
+          __typename
+          url
+        }
+      }
+    }
+    title
+  }
+}`)
+    throw e
+  }
+
+  return result
+}
+  
