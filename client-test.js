e4ac12157ee878f5c4d1660ecb5353b5aab99f8d
Alex Hoyau <a.hoyau@silexlabs.org>
2024-01-15 17:51:28 +0300

    ncu

--- client-test.js
+++ client-test.js
@@ -32,9 +32,25 @@ export default function (config, options = {}) {
     ],
     image: true,
     i18n: true,
+    filters: [
+      'generic',
+      'liquid',
+      {
+        type: 'filter',
+        id: 'Test custom filter',
+        label: 'Test custom filter',
+        validate: (field) => !!field,
+        output: type => type,
+        apply: (str) => {
+          console.info(str)
+          return str
+        },
+        options: {},
+        quotedOptions: [],
+      },
+    ],
     ...options,
   }
-  //config.addPlugin('../client.js', opts)
   config.addPlugin(EleventyPlugin, opts)
   return opts
 }
