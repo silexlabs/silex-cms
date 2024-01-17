75e47353bf19edc94218eb5cce73e30506c9b653
Alex Hoyau <a.hoyau@silexlabs.org>
2024-01-17 15:42:43 +0300

    ncu

--- 11ty-test/hosting/index.11tydata.js
+++ 11ty-test/hosting/index.11tydata.js
@@ -4,7 +4,7 @@ module.exports = async function () {
   const result = {}
   
   try {
-    result['directus'] = (await EleventyFetch(`https://eco-starter.2.internet2000.net/cms/graphql?cache_buster=330407`, {
+    result['directus'] = (await EleventyFetch(`https://eco-starter.2.internet2000.net/cms/graphql?cache_buster=406169`, {
       ...{"duration":"1s","type":"json"},
       fetchOptions: {
         headers: {
@@ -17,22 +17,7 @@ module.exports = async function () {
   blog {
     __typename
     label
-    translations {
-      __typename
-      title
-      richtext_content
-      image {
-        __typename
-        filename_download
-
-      }
-      languages_id {
-        __typename
-        code
-
-      }
-
-    }
+    id
 
   }
 
@@ -41,63 +26,12 @@ module.exports = async function () {
       }
     })).data
   } catch (e) {
-    console.error('11ty plugin for Silex: error fetching graphql data', e, 'directus', 'https://eco-starter.2.internet2000.net/cms/graphql?cache_buster=330407', 'POST', `query {
+    console.error('11ty plugin for Silex: error fetching graphql data', e, 'directus', 'https://eco-starter.2.internet2000.net/cms/graphql?cache_buster=406169', 'POST', `query {
   __typename
   blog {
     __typename
     label
-    translations {
-      __typename
-      title
-      richtext_content
-      image {
-        __typename
-        filename_download
-
-      }
-      languages_id {
-        __typename
-        code
-
-      }
-
-    }
-
-  }
-
-}`)
-    throw e
-  }
-
-
-  try {
-    result['sanity'] = (await EleventyFetch(`https://gl93e3h9.api.sanity.io/v2023-08-01/graphql/production/default?cache_buster=261035`, {
-      ...{"duration":"1s","type":"json"},
-      fetchOptions: {
-        headers: {
-          'Authorization': `Bearer skGP3qsWCXgPPTqmvIyAdeFhACwWKt94Eq3oCexQNvalA291L03mfLLq0DFHN9pVxhPuTVSs5vfDucKpWZHKRGGKwBdou4QYW32yNBBkHJjl2QGnn67KtdaqRCo7vhxlBObbZdFVylCgtO2M6c4lvkal0owBpVO4PCcMxeUC38z1r8pjvTXJ`,
-'content-type': `application/json`,
-        },
-        method: 'POST',
-        body: JSON.stringify({
-          query: `query {
-  __typename
-  Blog {
-    __typename
-
-
-  }
-
-}`,
-        })
-      }
-    })).data
-  } catch (e) {
-    console.error('11ty plugin for Silex: error fetching graphql data', e, 'sanity', 'https://gl93e3h9.api.sanity.io/v2023-08-01/graphql/production/default?cache_buster=261035', 'POST', `query {
-  __typename
-  Blog {
-    __typename
-
+    id
 
   }
 
