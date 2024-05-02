fec0441da491c466be4d392d7925ac1d4da4a091
Alex Hoyau <a.hoyau@silexlabs.org>
2024-05-02 12:10:58 +0500

    in app doc

--- src/client/site-settings.ts
+++ src/client/site-settings.ts
@@ -12,8 +12,8 @@ export default function(config: ClientConfig, opts: EleventyPluginOptions): void
         <div id="settings-cms" class="silex-hideable silex-hidden">
           <div class="gjs-sm-sector-title">Silex CMS</div>
           <div class="silex-help">
-            <p>The <a href="https://github.com/silexlabs/silex-cms">Silex CMS feature</a> integrates with your favorite headless CMS, API or database.</p>
-            <p>By adding data sources to your website you activate <a href="https://www.11ty.dev/docs/">11ty static site generator</a> integration. When you wil publish your website, the generated files assume you build the site with 11ty and possibly with Gitlab pages.</p>
+            <p>The <a target="_blank" href="https://github.com/silexlabs/silex-cms">Silex CMS feature</a> integrates with your favorite headless CMS, API or database.</p>
+            <p>By adding data sources to your website you activate <a target="_blank" href="https://www.11ty.dev/docs/">11ty static site generator</a> integration. When you wil publish your website, the generated files assume you build the site with 11ty and possibly with Gitlab pages.</p>
           </div>
             ${opts.view?.settingsEl ? (opts.view.settingsEl as () => HTMLElement)() : ''}
           </div>
